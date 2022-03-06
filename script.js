429d386a335ab1ff3211a3ca247fce09f937cc48
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-03-06 22:32:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+ const search = document.getElementById("search");
+        const results = document.getElementById("results");
+        let search_term = "";
 
+const showList = () => {
+            results.innerHTML = "";
+            data
+
+                .filter((item) => {
+                    return (
+                        item.number.toLowerCase().includes(search_term) ||
+                        item.brand.toLowerCase().includes(search_term)
+                    );
+                })
+
+                .forEach((e) => {
+                    const li = document.createElement("li");
+                    li.innerHTML = `${e.brand}  -  ${e.number}`;
+                    results.appendChild(li);
+                });
+        };
+
+        showList();
+
+        search.addEventListener("input", (event) => {
+            search_term = event.target.value.toLowerCase();
+            showList();
+        });
