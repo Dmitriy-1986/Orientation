24261ec5dedd7a3f13f87e9b3e0d8897d77993fa
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-03-08 19:38:27 +0200

    Update data.js

--- data.js
+++ data.js
@@ -130,7 +130,7 @@
             { img: "./images/1170.png", brand: "УАЗ", number: "1170" },
             { img: "./images/ВЕ7046ЕМ.png", brand: "Mitsubishi", number: "ВЕ 7046 ЕМ" },
             { img: "./images/BE7549EM.png", brand: "Mitsubishi", number: "BE 7549 EM" },
-            { img: "./images/AA2300OE.png", brand: "Mercedes", number: "AA 2300 OE" },
+            { img: "./images/AA2300OE.png", brand: "Mercedes-Benz ML 400 4 Matic", number: "AA 2300 OE" },
             { img: "./images/АА7785ВР.png", brand: "Volkswagen", number: "АА 7785 ВР" },
             { img: "./images/КА5257ВІ.png", brand: "Renault", number: "КА 5257 ВІ" },
             { img: "./images/КА0697СС.png", brand: "Skoda", number: "КА 0697 СС" },
