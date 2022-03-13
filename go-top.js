2b59279b8ca6aec0aed5cd24e5e25979765c81d5
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-03-13 13:56:59 +0200

    Update go-top.js

--- go-top.js
+++ go-top.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('body').append('<a href="#" id="go-top" title="Вверх"><i class="bi bi-arrow-up-square"></i></a>');
+  $('body').append('<a href="#" id="go-top"><img src="./images/arrow-up-square.svg"></a>');
 });
 
 $(function() {
