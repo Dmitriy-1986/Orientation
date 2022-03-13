46136e544d22f9f6914dfc98836a4454ed66c98a
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-03-13 21:07:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -27,11 +27,3 @@ search.addEventListener("input", (event) => {
     showList();
 });
 
-//Preloader
-window.onload = function () {
-      document.body.classList.add('loaded_hiding');
-      window.setTimeout(function () {
-        document.body.classList.add('loaded');
-        document.body.classList.remove('loaded_hiding');
-      }, 500);
-}
