e1f36512e6781e71fb8501b3ccdb2582a9d96b76
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-03-13 15:39:01 +0200

    Update go-top.js

--- go-top.js
+++ go-top.js
@@ -4,7 +4,7 @@ var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
 
 function handleScroll() {
   var scrollableHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var GOLDEN_RATIO = 0.8;
+  var GOLDEN_RATIO = 0.2;
 
   if ((document.documentElement.scrollTop / scrollableHeight ) > GOLDEN_RATIO) {
     //show button
