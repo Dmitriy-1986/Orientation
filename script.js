9bd902901acb21a574eb1125ab60557db0919380
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-03-13 16:19:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const showList = () => {
 
                 .forEach((e) => {
                     const li = document.createElement("li");
-                    li.innerHTML = `<img class="img-size" data-src="${e.img}">  ${e.brand},  <b class="text-primary"> ${e.number}</b>`;
+                    li.innerHTML = `<img class="img-size" src="${e.img}">  ${e.brand},  <b class="text-primary"> ${e.number}</b>`;
                     results.appendChild(li);
                 });
         };
