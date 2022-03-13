df4ce84056b3b2fbe2d5ec8eb07ce47090a2ad36
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-03-13 14:28:05 +0200

    Update go-top.js

--- go-top.js
+++ go-top.js
@@ -1,38 +1,13 @@
-var btn = $('#go-top');
-
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll').fadeIn(); 
+        } else { 
+            $('#scroll').fadeOut(); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
 });
-
-btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
-});
-/*
-$(document).ready(function(){
-  $('body').append('<a href="#" id="go-top"><img src="./images/arrow-up-square.svg"></a>');
-});
-
-$(function() {
- $.fn.scrollToTop = function() {
-  $(this).hide().removeAttr("href");
-  if ($(window).scrollTop() >= "250") $(this).fadeIn("slow")
-  var scrollDiv = $(this);
-  $(window).scroll(function() {
-   if ($(window).scrollTop() <= "250") $(scrollDiv).fadeOut("slow")
-   else $(scrollDiv).fadeIn("slow")
-  });
-  $(this).click(function() {
-   $("html, body").animate({scrollTop: 0}, "slow")
-  })
- }
-});
-
-$(function() {
- $("#go-top").scrollToTop();
-});
-*/
