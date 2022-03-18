e0857be62dcb0f5395fd71795674631803292494
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-03-18 19:01:34 +0200

    Update data.js

--- data.js
+++ data.js
@@ -242,7 +242,7 @@
             { img: "./images/no-image.png", brand: "Porshe Macan", number: "AX 7761 KE" },
             { img: "./images/no-image.png", brand: "Peugeot 301", number: "КА 5360 АМ" },
             { img: "./images/no-image.png", brand: "Renault Scenic", number: "КА 2425 СО" },
-            //{ img: "./images/no-image.png", brand: " ", number: " " }, 
+            { img: "./images/no-image.png", brand: "Audi Q3", number: "АН 5855 ІХ" }, 
             //{ img: "./images/no-image.png", brand: " ", number: " " }, 
             //{ img: "./images/no-image.png", brand: " ", number: " " }, 
             //{ img: "./images/no-image.png", brand: " ", number: " " },
