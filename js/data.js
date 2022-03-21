ac9880dd5519cab09b84bdf2bef69f96ac50e56f
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-03-21 13:48:20 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -268,7 +268,7 @@ const data = [
             { img: "./images/no-image.png", brand: "Citroen Berlingo", number: "KA 3814 EM" },
             { img: "./images/no-image.png", brand: "Citroen Berlingo", number: "KA 3866 EM" },
             { img: "./images/АР9434ІС.png", brand: "Volkswagen T6", number: "АР 9434 ІС" },
-            //{ img: "./images/no-image.png", brand: " ", number: " " }, 
+            { img: "./images/АР0858ЕТ.png", brand: "Mitsubishi Outlander", number: "АР0858ЕТ" }, 
             //{ img: "./images/no-image.png", brand: " ", number: " " },
             //{ img: "./images/no-image.png", brand: " ", number: " " },
             //{ img: "./images/no-image.png", brand: " ", number: " " },
