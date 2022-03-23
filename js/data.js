2df2a9cf030d8859a66829d104a252b5f8871a34
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-03-23 08:49:50 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -275,7 +275,7 @@ const data = [
             { img: "./images/no-image.webp", brand: "Toyota Camry", number: "КА 2791 НВ" },
             { img: "./images/no-image.webp", brand: "Chery E5 ", number: "AP 1978 EP" },
             { img: "./images/no-image.webp", brand: "Toyota Land Cruiser", number: "AP 5595 AE" },
-            //{ img: "./images/no-image.webp", brand: " ", number: " " },
+            { img: "./images/no-image.webp", brand: "ВАЗ 21130", number: "AP 5544 CT" },
             //{ img: "./images/no-image.webp", brand: " ", number: " " }, 
             //{ img: "./images/no-image.webp", brand: " ", number: " " }, 
             //{ img: "./images/no-image.webp", brand: " ", number: " " }, 
