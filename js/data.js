64d5bd8bd80e39206eff05140bfd2623e9060f02
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-03-25 06:01:23 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -279,7 +279,7 @@ const data = [
             { img: "./images/no-image.webp", brand: "Mitsubishi L200 ", number: "AP 7141 HT" }, 
             { img: "./images/E8217KI.png", brand: "Audi", number: "E 8217 KI" }, 
             { img: "./images/no-image.webp", brand: "Renault Logan", number: "АІ 4998 НІ" }, 
-            //{ img: "./images/no-image.webp", brand: " ", number: " " },
+            { img: "./images/no-image.webp", brand: "Skoda Karoq", number: "АР 5888 АВ" },
             //{ img: "./images/no-image.webp", brand: " ", number: " " },
             //{ img: "./images/no-image.webp", brand: " ", number: " " },
             //{ img: "./images/no-image.webp", brand: " ", number: " " },
