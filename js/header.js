c292453014d7f45e180fc274d04c373ae3b829a2
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-03-29 21:28:50 +0300

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,1 +1,24 @@
+let header = document.getElementById('header');
+let marquee = document.createElement('marquee');
+marquee.setAttribute('loop', '3');
+marquee.setAttribute('behavior', 'scroll');
+header.appendChild(marquee);
+let strongMarqueeStr = document.createElement('strong');
+strongMarqueeStr.innerHTML = `Увага! Особи за орієнтуванням при собі можуть
+                            мати автоматичну та іншу зброю. При затриманні 
+                            дотримуватися заходів особистої безпеки!`;
+marquee.appendChild(strongMarqueeStr);
 
+let titleOrientation = document.createElement('h2');
+titleOrientation.setAttribute('title', 'ОРІЄНТУВАННЯ');
+titleOrientation.classList.add('tracking-in-expand');
+titleOrientation.innerHTML = 'ОРІЄНТУВАННЯ';
+header.appendChild(titleOrientation);
+
+let inputSearch = document.createElement('input');
+inputSearch.id = "search"; 
+inputSearch.autofocus = true;
+inputSearch.placeholder =  'Шукати по орієнтуванням...';
+inputSearch.setAttribute('autocomplete', 'off');
+inputSearch.setAttribute('type', 'search');
+header.appendChild(inputSearch);
