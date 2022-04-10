cda0e5d570c1e89f0c2a3c896e6760fdf09034a3
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-04-10 14:32:07 +0300

    Update header.js

--- js/header.js
+++ js/header.js
@@ -30,7 +30,7 @@ const now = new Date().toLocaleString();
 let footer = document.getElementById('footer');
 let footerInfo = document.createElement('p');
 footerInfo.innerHTML = 'Всього: <span style=\'color: #dc3545;\'>' + data.length + '</span> орієнтувань, ';
-footerInfo.innerHTML += 'станом на ' + now;
+footerInfo.innerHTML += 'станом на: ' + now + ', для перевірки оновлень орієнтувань, перезавантажте браузер.';
 footer.appendChild(footerInfo);
 
 
