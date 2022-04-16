a2f9792132997a65994e7bc21b3f082a588fa744
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-04-16 09:49:54 +0300

    Update telegramLink.js

--- js/telegramLink.js
+++ js/telegramLink.js
@@ -1,9 +1,4 @@
-let telegramLink = document.createElement('a');
-telegramLink.id = 'telegramLink';
-telegramLink.className = 'messenger-links';
-telegramLink.setAttribute('title', 'Telegram');
-telegramLink.setAttribute('href', 'https://t.me/patrol_police_zp');
-telegramLink.setAttribute('href', 'https://t.me/patrol_police_zp');
-let a = `<a id="telegramLink" class="messenger-links" title="Telegram" href="https://t.me/patrol_police_zp" target="_blank">
+let telegramLink = `<a id="telegramLink" class="messenger-links" title="Telegram" href="https://t.me/patrol_police_zp" target="_blank">
             <img src="./images/telegram-icon.svg" alt="Telegram">
-</a>`
+         </a>`;
+body.appendChild(telegramLink);
