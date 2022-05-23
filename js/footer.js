2ed13cc8dfa96d387a6e07bf36b3f917c234cd67
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-05-23 11:41:08 +0300

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,10 +1,18 @@
 const now = new Date().toLocaleString();
 let modified = document.lastModified;
 
+let dateTime = document.getElementById('clock');
+function clock() {
+   dateTime.innerHTML = 'Поточна дата та час: <b style=\'color: #dc3545;\'>' + now + '</b>. ';
+}
+
+setInterval(clock, 1000);
+
 let footer = document.getElementById('footer');
 let footerInfo = document.createElement('p');
 footerInfo.innerHTML = 'Всього орієнтувань: <b style=\'color: #dc3545;\'>' + data.length + '</b>. ';
 footerInfo.innerHTML += 'Останні зміни сайту були здійсненні: <b style=\'color: #dc3545;\'>' + modified + '</b>. ';
 footerInfo.innerHTML += 'Веб сторінка була завантажена: <b style=\'color: #dc3545;\'>' + now + '</b>. ';
+footerInfo.innerHTML += '<span id=\'clock\'></span>';
 footerInfo.innerHTML += 'Для перевірки оновлень орієнтувань, будь ласка перезавантажте сайт.';
 footer.appendChild(footerInfo);
