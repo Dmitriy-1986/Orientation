ee8e8f6e83ba17d85deb0e16de5d956c19542ee2
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-05-23 10:59:01 +0300

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -3,7 +3,7 @@ let modified = document.lastModified;
 
 let footer = document.getElementById('footer');
 let footerInfo = document.createElement('p');
-footerInfo.innerHTML = 'Всього: <span style=\'color: #dc3545;\'><b>' + data.length + '</b></span> орієнтувань. ';
+footerInfo.innerHTML = 'Всього орієнтувань: <span style=\'color: #dc3545;\'><b>' + data.length + '</b></span>. ';
 footerInfo.innerHTML += 'Останні зміни сайту були здійсненні: <b>' + modified + '</b>. ';
 footerInfo.innerHTML += 'Веб сторінка була завантажена: <b>' + now + '</b>. ';
 footerInfo.innerHTML += 'Для перевірки оновлень орієнтувань, будь ласка перезавантажте веб-сторінку.';
