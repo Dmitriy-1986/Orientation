782abbc522672300a1498482f913e5c8a5c35cac
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-05-25 07:40:37 +0300

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -4,8 +4,8 @@ let modified = document.lastModified;
 let footer = document.getElementById('footer');
 let footerInfo = document.createElement('p');
 footerInfo.innerHTML = 'Всього орієнтувань: <b style=\'color: #dc3545;\'>' + data.length + '</b>. ';
-footerInfo.innerHTML += 'Останні зміни сайту були здійсненні: <b style=\'color: #dc3545;\'>' + modified + '</b>. ';
-footerInfo.innerHTML += 'Веб сторінка була завантажена: <b style=\'color: #dc3545;\'>' + now + '</b>. ';
+footerInfo.innerHTML += 'Останні зміни сайту були здійсненні: <b>' + modified + '</b>. ';
+footerInfo.innerHTML += 'Веб сторінка була завантажена: <b>' + now + '</b>. ';
 footerInfo.innerHTML += '<span id=\'clock\'></span>';
 footerInfo.innerHTML += 'Для перевірки оновлень орієнтувань, будь ласка перезавантажте сайт.';
 footer.appendChild(footerInfo);
@@ -14,7 +14,7 @@ let dateTime = document.getElementById('clock');
 function clock() {
    const date = new Date();
    let time = date.toLocaleString();
-   dateTime.innerHTML = 'Поточна дата та час: <b style=\'color: #dc3545;\'>' + time + '</b>. ';
+   dateTime.innerHTML = 'Поточна дата та час: <b>' + time + '</b>. ';
 }
 
 setInterval(clock, 1000);
