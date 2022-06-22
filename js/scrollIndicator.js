6e7545873908f20fa2cd0d045c655b76ae629cae
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-06-22 10:31:33 +0300

    Update scrollIndicator.js

--- js/scrollIndicator.js
+++ js/scrollIndicator.js
@@ -2,7 +2,7 @@ window.onload = () => {
 
     const progress = () => { 
         const line = document.createElement('div')
-        line.className = 'progress'
+        line.className = 'progress';
         line.style.cssText = `
                             height: 6px;
                             background: linear-gradient(to right, #7b4397, #dc2430);
@@ -11,31 +11,31 @@ window.onload = () => {
                             left: 0;
                             transition: 1s;
                             z-index: 99999;
-                        `
-        document.body.prepend(line) 
+                        `;
+        document.body.prepend(line); 
 
         const progressWidth = () => { 
-            return line.style.width = Math.floor(window.pageYOffset / (document.documentElement.scrollHeight - window.innerHeight) * 100) + '%'
+            return line.style.width = Math.floor(window.pageYOffset / (document.documentElement.scrollHeight - window.innerHeight) * 100) + '%';
         }
 
-        progressWidth()
+        progressWidth();
 
-        document.addEventListener('scroll', throttle(progressWidth, 64)) 
-        window.addEventListener('resize', throttle(progressWidth, 64))
+        document.addEventListener('scroll', throttle(progressWidth, 64)); 
+        window.addEventListener('resize', throttle(progressWidth, 64));
 
     }
 
-    progress() 
+    progress(); 
 }
 
 const throttle = (func, ms) => { 
-    let locked = false 
+    let locked = false; 
     return () => { 
-        if (locked) return 
-        locked = true 
+        if (locked) return; 
+        locked = true; 
         setTimeout(() => { 
-            func() 
-            locked = false 
-        }, ms) 
+            func(); 
+            locked = false; 
+        }, ms); 
     }
 }
