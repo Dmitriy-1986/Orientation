31264c9f4a7a1678f8a40fd9aed966ca96b5d4d2
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-08-31 20:58:40 +0300

    Update header.js

--- js/header.js
+++ js/header.js
@@ -19,6 +19,7 @@ legend.appendChild(small);
 let inputSearch = document.createElement('input');
 inputSearch.id = "search"; 
 inputSearch.autofocus = true;
+inputSearch.style.caretColor = 'red';
 inputSearch.placeholder =  'Шукати по орієнтуванням...';
 inputSearch.setAttribute('autocomplete', 'off');
 inputSearch.setAttribute('type', 'search');
